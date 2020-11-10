a75a3eef7c4eaae0ebf14ce64c7008c05608a59a
Nicholas B Kriz <nkriz@clemson.edu>
2020-11-10 18:19:26 -0500

    created dataset of top 500 on marketcap

--- coinmarketcap_request.py
+++ coinmarketcap_request.py
@@ -6,16 +6,56 @@ import time
 if not os.path.exists("midterm_html_files"):
 	os.mkdir("midterm_html_files")
 
-for i in range(15):
-	current_time_stamp = datetime.datetime.fromtimestamp(time.time()).strftime("%Y%m%d%H%M%S")
-	print(current_time_stamp)
+# for i in range():
+current_time_stamp = datetime.datetime.fromtimestamp(time.time()).strftime("%Y%m%d%H%M%S")
+print(current_time_stamp)
+f = open("midterm_html_files/coinmarketcap" + current_time_stamp + ".html", "wb")
+coin_market_response = urllib.request.urlopen("https://coinmarketcap.com/")
+coin_market_html = coin_market_response.read()
 
-	f = open("midterm_html_files/coinmarketcap" + current_time_stamp + ".html", "wb")
+f.write(coin_market_html)
+f.close()
 
-	coin_market_response = urllib.request.urlopen("https://coinmarketcap.com/")
-	coin_market_html = coin_market_response.read()
+time.sleep(15)
 
-	f.write(coin_market_html)
-	f.close()
-	
-	time.sleep(10)
+current_time_stamp = datetime.datetime.fromtimestamp(time.time()).strftime("%Y%m%d%H%M%S")
+print(current_time_stamp)
+f = open("midterm_html_files/coinmarketcap" + current_time_stamp + ".html", "wb")
+coin_market_response = urllib.request.urlopen("https://coinmarketcap.com/2")
+coin_market_html = coin_market_response.read()
+
+f.write(coin_market_html)
+f.close()
+
+time.sleep(15)
+
+current_time_stamp = datetime.datetime.fromtimestamp(time.time()).strftime("%Y%m%d%H%M%S")
+print(current_time_stamp)
+f = open("midterm_html_files/coinmarketcap" + current_time_stamp + ".html", "wb")
+coin_market_response = urllib.request.urlopen("https://coinmarketcap.com/3")
+coin_market_html = coin_market_response.read()
+
+f.write(coin_market_html)
+f.close()
+
+time.sleep(15)
+
+current_time_stamp = datetime.datetime.fromtimestamp(time.time()).strftime("%Y%m%d%H%M%S")
+print(current_time_stamp)
+f = open("midterm_html_files/coinmarketcap" + current_time_stamp + ".html", "wb")
+coin_market_response = urllib.request.urlopen("https://coinmarketcap.com/4")
+coin_market_html = coin_market_response.read()
+
+f.write(coin_market_html)
+f.close()
+
+time.sleep(15)
+
+current_time_stamp = datetime.datetime.fromtimestamp(time.time()).strftime("%Y%m%d%H%M%S")
+print(current_time_stamp)
+f = open("midterm_html_files/coinmarketcap" + current_time_stamp + ".html", "wb")
+coin_market_response = urllib.request.urlopen("https://coinmarketcap.com/5")
+coin_market_html = coin_market_response.read()
+
+f.write(coin_market_html)
+f.close()
--- coinrank_request.py
+++ coinrank_request.py
@@ -6,16 +6,15 @@ import time
 if not os.path.exists("midterm_html_g_files"):
 	os.mkdir("midterm_html_g_files")
 
-for i in range(15):
-	current_time_stamp = datetime.datetime.fromtimestamp(time.time()).strftime("%Y%m%d%H%M%S")
-	print(current_time_stamp)
+	for i in range():
+		current_time_stamp = datetime.datetime.fromtimestamp(time.time()).strftime("%Y%m%d%H%M%S")
+		print(current_time_stamp)
 
-	f = open("midterm_html_g_files/coinrank" + current_time_stamp + ".html", "wb")
+		f = open("midterm_html_g_files/coinrank" + current_time_stamp + ".html", "wb")
+		coin_rank_response = urllib.request.urlopen("https://coinranking.com/")
+		coin_rank_html = coin_rank_response.read()
 
-	coin_rank_response = urllib.request.urlopen("https://coinranking.com/")
-	coin_rank_html = coin_rank_response.read()
-
-	f.write(coin_rank_html)
-	f.close()
-	
-	time.sleep(15)
+		f.write(coin_rank_html)
+		f.close()
+		
+		time.sleep(15)
